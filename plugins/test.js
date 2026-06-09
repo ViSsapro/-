1a67c62ee909bcdeb346ebc3e1f58739d82db204
~ɢᴏᴅ ʀᴀꜰᴀᴇʟ <vimukthithuhina754@gmail.com>
2026-06-09 12:24:25 +0530

    Delete plugins directory

--- plugins/test.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'test',
-    commands: ['test', 'menu'],
-    execute: async (sock, mek, args, from) => {
-        await sock.sendMessage(from, { text: "✅ THUHI MD වැඩ කරයි! ප්ලගින් පද්ධතිය සාර්ථකයි." }, { quoted: mek });
-    }
-};
